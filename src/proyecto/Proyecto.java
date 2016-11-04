1e961a7f0647dfe28d25e09ecabcce28281c2f3a
alumne <alumne@alumne-iesebre>
2016-11-04 09:49:43 +0100

    Casi acabat

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -7,9 +7,15 @@ public class Proyecto {
     public static void main(String[] args) {
 
         // TODO code application logic here
-        int opcio, nota1, nota2, mp, ms, mostrar;
+        int opcio, nota1, nota2, mp, ms, mostrar, totm;
         boolean omplit = false, estat;
-        String nom, nomCicle ;
+        String nom, nomCicle;
+        nom = "sdas";
+        nomCicle = "dasda";
+        mp = 0;
+        ms = 0;
+        nota1 = 0;
+        nota2 = 0;
 
         Scanner ent = new Scanner(System.in);
 
@@ -20,10 +26,10 @@ public class Proyecto {
 
             switch (opcio) {
                 case 1:
-                    System.out.println("HAS ESCOLLIT LA OPCIO DE AFEGIR ALUMNE\n\n\n");
+                    System.out.println("HAS ESCOLLIT LA OPCIO DE AFEGIR ALUMNE\n\n");
                     System.out.println("___________________________________________");
                     System.out.println("___________________________________________");
-                    if (omplit==false) {
+                    if (omplit == false) {
 
                         System.out.print("Introdueix Nom de Alumne: \n");
                         nom = ent.skip("[\r\n]*").nextLine();
@@ -33,43 +39,93 @@ public class Proyecto {
 
                         System.out.print("Introdueix Numero de moduls Aprovats: \n");
                         mp = ent.nextInt();
-                        
+
                         System.out.print("Introdueix la Nota mes alta de moduls Aprovats: \n");
                         nota1 = ent.nextInt();
 
                         System.out.print("Introdueix Numero del Moduls Suspesos: \n");
                         ms = ent.nextInt();
-                        
-                        System.out.print("Introdueix la Nota mes Baixa de moduls Suspesos :\n" );
+
+                        System.out.print("Introdueix la Nota mes Baixa de moduls Suspesos :\n");
                         nota2 = ent.nextInt();
                         System.out.println("\n");
                         System.out.println("___________________________________________");
                         System.out.println("___________________________________________");
-                        
+
                         omplit = true;
-                    } else{
+                    } else {
                         System.out.println("Ja tens Dades Introduides\n");
-                        System.out.println("Vols Mostrar o Llistar Les Dades Guardades?( 1. Si \n 2- No ):\n");
+                        System.out.println("Vols Mostrar o Llistar Les Dades Guardades?( 1.Si o 2.No ):\n");
                         mostrar = ent.nextInt();
-                        if (mostrar == 1) {
-                            System.out.println("Nom Alumne:" (+nom));
+                        switch (mostrar) {
+                            case 1:
+                                System.out.println("-------------------------------------------------------------------");
+                                System.out.println("| Nom Alumne: " + (nom));
+                                System.out.println("| Nom Curs: " + (nomCicle));
+                                System.out.format(" | Numero de moduls Aprovats:  %s   i la nota Més alta: %s", mp, nota1);
+                                System.out.format(" | Numero de moduls Suspesos:  %s   i la nota Més alta: %s", ms, nota2);
+                                System.out.println("-------------------------------------------------------------------");
+                                break;
+                            default:
+                                break;
                         }
                     }
-                    
 
                     break;
                 case 2:
-                    System.out.println("HAS ESCOLLIT LA OPCIO DE EDITAR\n\n\n");
+                    System.out.println("HAS ESCOLLIT LA OPCIO DE EDITAR\n\n");
+                    System.out.println("-------------------------------------------------------------------");
+                    System.out.println("| Nom Alumne: " + (nom));
+                    System.out.println("| Introduix El Nom Editat: ");
+                    nom = ent.skip("[\r\n]*").nextLine();
+                    System.out.println("| Nom Curs: " + (nomCicle));
+                    System.out.print("  | Introdueix Nom de Cicle Editat:\n ");
+                    nomCicle = ent.skip("[\r\n]*").nextLine();
+                    System.out.format(" | Numero de moduls Aprovats:  %s   i la nota Més alta: %s  \n", mp, nota1);
+                    System.out.print(" |Introdueix Numero de moduls Aprovats:  i la nota més Alta \n");
+                    mp = ent.nextInt();
+                    nota1 = ent.nextInt();
+                    System.out.format(" | Numero de moduls Suspesos:  %s   i la nota Més alta: %s", ms, nota2);
+                    System.out.print(" | Introdueix Numero de moduls Supesos:  i la nota més baixa \n");
+                    ms = ent.nextInt();
+                    nota2 = ent.nextInt();
+
                     break;
                 case 3:
-                    System.out.println("HAS ESCOLLIT LA OPCIO DE LLISTAR\n\n\n");
-                    System.out.println("Nom de Alumne: "+ ( nom + "\n\n"));
-                    
-                    
-                    
+                    totm=mp+ms;
+                    if (nota1>nota2 && mp>ms) {
+                       estat=true;                         
+                    }else{ estat=false;}
+                    System.out.println("HAS ESCOLLIT LA OPCIO DE LLISTAR\n\n");                    
+                    System.out.println("-------------------------------------------------------------------");
+                    System.out.println("| Nom Alumne: " + (nom));
+                    System.out.println("| Nom Curs: " + (nomCicle));
+                    System.out.format(" | Numero de moduls Aprovats:  %s   i la nota Més alta: %s", mp, nota1);
+                    System.out.format(" | Numero de moduls Suspesos:  %s   i la nota Més alta: %s", ms, nota2);
+                    System.out.println("| la Nota mitjana mes aproximada es:"+((mp+ms)));
+                    System.out.println("-------------------------------------------------------------------");
+
                     break;
                 case 4:
                     System.out.println("HAS ESCOLLIT LA OPCIO DE BORRAR\n\n\n");
+                    System.out.println("-------------------------------------------------------------------");
+                    System.out.println("-------------- Ojo Esteu Segur de Borrar Tots els Registres? 1-Si o 2-No");
+                    System.out.println("intodueix una opcio:");
+                    mostrar = ent.nextInt();
+                    switch (mostrar) {
+                            case 1:
+                                omplit = false;
+                                System.out.println("----------------------------------");
+                                System.out.println("| Nom Alumne: Borrat!! " );
+                                System.out.println("| Nom Curs: Borrat!! ");
+                                System.out.println("| Moduls aprovats i Nota: Borrats!! ");                                
+                                System.out.println("| Moduls suspesos  i nota : Borrats!! ");
+                                System.out.println("---------------Dades borrrades 100%!! ");
+                                
+                                break;
+                            default:
+                                break;
+                        }
                     break;
                 case 5:
                     System.out.println("Aquest Programa Es Creat Per:\n");
@@ -83,8 +139,6 @@ public class Proyecto {
                 default:
                     System.out.println("Perfavor Introdueix una de les OPCIONS\n\n\n  ");
             }
-           
-            
 
         } while (opcio != 6);
     }
